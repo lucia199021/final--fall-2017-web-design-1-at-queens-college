ea96fcadb1fae166feb2f5cb671535bd049dec3e
lucia199021 <31673406+lucia199021@users.noreply.github.com>
2017-12-20 10:53:58 +1200

    final_LuciaRivera

---
+++ lucia-rivera--lucia199021/Final/assets/js/main.js
@@ -0,0 +1,16 @@
+(function($) {
+    "use strict";
+        jQuery(document).ready(function ($) {
+  
+   /* 
+    -----------------------
+    PrettyPhoto Js
+    -----------------------
+    */         
+       $("a[rel^='prettyPhoto']").prettyPhoto();     
+            
+       
+    }); // Ready
+    
+    
+})(jQuery);
